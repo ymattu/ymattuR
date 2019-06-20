abdb8ecdb30ed6d45f746b9a30f589c6c07dbdf3
Yuya Matsumura <mattu.yuya@gmail.com>
2019-06-20 18:56:20 +0900

    fix html path

--- R/rmd.R
+++ R/rmd.R
@@ -78,7 +78,7 @@ rmd_template <- function(file, path = ".", type = "html", systime = TRUE,
   if(katex == T) {
     katex_html <- system.file("inst/include_katex.html", package = "ymattuR")
     katex_c <- paste0("\n    mathjax: NULL\n    includes:\n      in_header: ",
-                      system.file("inst/include_katex.html", package = "ymattuR"))
+                      system.file("iinclude_katex.html", package = "ymattuR"))
   } else {
     katex_c <- NULL
   }
