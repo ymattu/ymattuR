53bde56c2ccded722a6bff9f600a76a184f7f0d4
Yuya Matsumura <mattu.yuya@gmail.com>
2020-05-23 10:36:09 +0900

    fix create_rproj.R  devtools -> usethis

---
+++ R/write.R
@@ -0,0 +1,18 @@
+#' Write to clipboard
+#'
+#' @param x dataframe or matrix
+#' @export
+write_clipboard <- function(x) {
+    os <- get_os()
+    if (os == "mac") {
+        if (is.data.frame(x) | is.matrix(x)) {
+            write.table(x, pipe("pbcopy"), sep = "\t", row.names = FALSE, quote = FALSE)
+        } else write.table(t(x), pipe("pbcopy"), sep = "\t", row.names = FALSE, quote = FALSE)
+    } else if (os == "win") {
+        if (is.data.frame(x) | is.matrix(x)) {
+            write.table(x, file = "clipboard", sep = "\t", row.names = FALSE, quote = FALSE)
+        } else write.table(t(x), file = "clipboard", sep = "\t", row.names = FALSE, quote = FALSE)
+    } else {
+        stop("This function is available only in Windows or Mac OS")
+    }
+}
