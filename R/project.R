5548c9769869321ae357716da6055aa3feef1128
Yuya Matsumura <mattu.yuya@gmail.com>
2020-05-23 10:24:02 +0900

    fix create_rproj.R  devtools -> usethis

--- R/project.R
+++ R/project.R
@@ -4,10 +4,10 @@
 create_proj_file <- function(dir) {
   path <- file.path(dir, paste0(basename(dir), ".Rproj"))
   template_path <- system.file("templates/template.Rproj",
-                               package = "devtools")
+                               package = "usethis")
   x <- c("Version: 1.0", "", "RestoreWorkspace: Default", "SaveWorkspace: Default",
          "AlwaysSaveHistory: Default", "", "EnableCodeIndexing: Yes",
-         "UseSpacesForTab: Yes", "NumSpacesForTab: 4", "Encoding: UTF-8",
+         "UseSpacesForTab: Yes", "NumSpacesForTab: 2", "Encoding: UTF-8",
          "", "RnwWeave: knitr", "LaTeX: pdfLaTeX")
 
   cat(paste(x, collapse="\n"), file = template_path)
